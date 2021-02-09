1529215d037a5dd1eff670e405f8d14d70782e2d
dicki sujadi <dickisujadi@gmail.com>
2021-02-09 09:33:22 +0700

    adding css, img, js folders and home.html

---
+++ js/function.js
@@ -0,0 +1,8 @@
+// JavaScript source code
+
+$(document).ready(function () {
+    $("#screen1").click(function () {
+        $(".text-box").fadeOut(500);
+        $("#screen1").fadeOut(1800);
+    });
+});
