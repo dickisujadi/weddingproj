becb08d7097f29282742f3b8ff57d22958080751
dicki sujadi <dickisujadi@gmail.com>
2021-02-13 23:37:16 +0700

    fixing animation

--- js/function.js
+++ js/function.js
@@ -10,3 +10,17 @@ $(document).ready(function () {
         $("#screen2").fadeIn(1900);
     });
 });
+
+var aos_container = document.querySelectorAll('.aos_container');
+
+for (let i = 0; i < aos_container.length; i++) {
+    window.addEventListener('scroll', (e) => {
+        var positionY = aos_container[i].getBoundingClientRect().top - (window.innerHeight / 2);
+        aos_container[i].style.position = 'relative';
+        if (positionY <= 0) {
+            aos_container[i].classList.add('active');
+        } else {
+            aos_container[i].classList.remove('active');
+        }
+    })
+}
