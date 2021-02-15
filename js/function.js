519a46d0e02f904ac36d72d79883af80eb39a3bb
dicki sujadi <dickisujadi@gmail.com>
2021-02-15 16:40:42 +0700

    fixing full website

--- js/function.js
+++ js/function.js
@@ -1,6 +1,12 @@
 // JavaScript source code
+$(function() {
+    $("#brown2").height($("#kami").height());
+});
 
 $(document).ready(function () {
+    // $('#kami').each(function() {
+    //     $('#brown2').setAttribute("style", "height:" + );
+    // })
     // $("#screen1").hide();
 
     $("#screen2").hide();
@@ -9,6 +15,38 @@ $(document).ready(function () {
         $("#screen1").fadeOut(1800);
         $("#screen2").fadeIn(1900);
     });
+
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(successFunction, errorFunction);
+    } else {
+        alert('It seems like Geolocation, which is required for this page, is not enabled in your browser. Please use a browser which supports it.');
+    }
+
+    function successFunction(position) {
+        var lat = position.coords.latitude;
+        var long = position.coords.longitude;
+        var position = lat + ',' + long;
+
+        $('#saddr').text(position);
+    }
+
+    function errorFunction() {
+        try {
+            lat = position.coords.latitude;
+            long = position.coords.longitude;
+        }
+        catch(error) {
+            console.log(error);
+        }
+    }
+
+    // var kami = document.getElementById('s-container5');
+    // console.log(kami.clientHeight);
+    // $(function() {
+        console.log($("#brown2").height());
+    //  })
+    // document.getElementById('brown2').setAttribute("style", "height: " + document.getElementById('kami').clientHeight);
+    // console.log("kami height " + document.getElementById('kami').clientHeight);
 });
 
 var aos_container = document.querySelectorAll('.aos_container');
